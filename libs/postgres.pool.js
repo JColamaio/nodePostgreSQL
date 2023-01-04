fed37051300ecc0d1193bebe4f9e2f574e103a33
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2023-01-04 16:43:54 -0300

    adding .env for security

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,13 +1,10 @@
 const { Pool } = require("pg")
+const { config } = require("../config/config")
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user:'Jonathan',
-  password:'admin123',
-  database:'postgres'
-});
+const USER = encodeURIComponent(config.dbUser)
+const PASSWORD = encodeURIComponent(config.dbPassword)
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
-
+const pool = new Pool({ connectionString: URI });
 
 module.exports = pool;
